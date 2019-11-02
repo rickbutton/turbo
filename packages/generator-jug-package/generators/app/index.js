808f0c6036f8f1db4516c532a896ae42ce71ac3c
Rick Button <rick@button.dev>
2019-11-01 23:19:58 -0400

    fixed generator

---
+++ packages/generator-jug-package/generators/app/index.js
@@ -0,0 +1,50 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require("path");
+
+module.exports = class extends Generator {
+    prompting() {
+        this.log(
+          yosay(`Welcome to the ${chalk.red('generator-jug-package')} generator!`)
+        );
+
+        const prompts = [
+            {
+                type: "input",
+                name: "name",
+                message: "project name",
+            },
+        ];
+
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+
+            this.destinationRoot(path.join("packages", props.name));
+        });
+    }
+
+    writing() {
+        const files = [
+            "package.json",
+            "src/index.ts",
+            "src/index.spec.ts",
+            "tsconfig.build.json",
+            "tsconfig.json",
+        ];
+
+        for (const file of files) {
+            this.fs.copyTpl(
+                this.templatePath(file),
+                this.destinationPath(file),
+                this.props,
+            );
+        }
+    }
+
+    install() {
+        this.log("make sure to re-bootstrap!");
+    }
+};
